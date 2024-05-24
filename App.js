d050b2b9e4cdb46bc42351addd63e913b4db1d4d
death-inspires-me <nana.esseney@gmail.com>
2024-05-24 10:43:58 +0000

    Linked to expo go, changed background colour

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fc6b3f',
     alignItems: 'center',
     justifyContent: 'center',
   },
